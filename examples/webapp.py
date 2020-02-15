25b1d5509b67bb145435ff94ab533e410ea3a39d
Tom Kwong <tk3369@gmail.com>
2020-02-14 23:04:37 -0800

    flask app example

---
+++ examples/webapp.py
@@ -0,0 +1,29 @@
+# A simle Flask app that serves a `format` endpoint
+#
+# How to run:
+# 1. Start from the project home directory
+# 2. Run the following commands:
+#      export FLASK_APP=examples/webapp.py
+#      python -m flask run
+
+import sqlparse
+
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/format', methods = ['POST'])
+def format():
+    sql = request.form['sql']
+
+    formatted_sql = sqlparse.format(sql,
+        reindent=True,
+        # strip_whitespace=True,
+        keyword_case='lower',
+        identifier_case='lower',
+        comma_first=True,
+        use_space_around_operators=True,
+        more_newlines=True,
+        # indent_width=4,
+        )
+
+    return formatted_sql.strip()
